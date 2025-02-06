b81eabf3fcbed1c65568d63a79880cb6c5c8e8ae
Cyrus <2213259@slu.edu.ph>
2025-02-07 05:00:20 +0800

    update MultiplesOf3or5Test.java

--- src/test/java/hello/MultiplesOf3or5Test.java
+++ src/test/java/hello/MultiplesOf3or5Test.java
@@ -7,10 +7,10 @@ public class MultiplesOf3or5Test {
     
     @Test
     public void testSolution() {
-        assertEquals(23, MultiplesOf3or5Test.solution(10));  
-        assertEquals(78, MultiplesOf3or5Test.solution(20));  
-        assertEquals(0, MultiplesOf3or5Test.solution(0));  
-        assertEquals(0, MultiplesOf3or5Test.solution(-5)); 
-        assertEquals(143, MultiplesOf3or5Test.solution(25));  
+        assertEquals(23, MultiplesOf3or5.solution(10));  
+        assertEquals(78, MultiplesOf3or5.solution(20));  
+        assertEquals(0, MultiplesOf3or5.solution(0));  
+        assertEquals(0, MultiplesOf3or5.solution(-5)); 
+        assertEquals(143, MultiplesOf3or5.solution(25));  
     }
 }
