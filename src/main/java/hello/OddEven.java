1a2266578a78f4d8d079939fd18ccb49c67ae70b
markuzfianza <2211954@slu.edu.ph>
2025-02-07 14:58:52 +0800

    Initial commit with TODO comments

--- src/main/java/hello/OddEven.java
+++ src/main/java/hello/OddEven.java
@@ -1,6 +1,9 @@
 package hello;
+
 public class OddEven {
     public static String oddOrEven(int number) {
-        return (number % 2 == 0) ? "Even" : "Odd";
+        // TODO: Implement logic to check if the number is odd or even
+        // TODO: Return "Even" if the number is even, otherwise return "Odd"
+        return null; // Placeholder return statement
     }
 }
