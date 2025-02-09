b3ca3735076a2fa9f47e2b08dafd541e6261c1ce
ReubenCablaida <2234208@slu.edu.ph>
2025-02-10 04:12:07 +0800

    Update DinglemouseTest.java
    Remove some statements on the file

--- src/test/java/hello/DinglemouseTest.java
+++ src/test/java/hello/DinglemouseTest.java
@@ -16,16 +16,5 @@ public class DinglemouseTest {
     @Test
     void testTenYears() {
         assertArrayEquals(new int[]{10, 56, 64}, Dinglemouse.humanYearsCatYearsDogYears(10));
-    public void testSingleDigitNumbers() {
-        assertEquals("One", Dinglemouse.getNumberName(1));
-        assertEquals("Two", Dinglemouse.getNumberName(2));
-        assertEquals("Three", Dinglemouse.getNumberName(3));
-        assertEquals("Zero", Dinglemouse.getNumberName(0));
-    }
-
-    @Test
-    public void testUnknownNumber() {
-        assertEquals("Unknown", Dinglemouse.getNumberName(10));
-        assertEquals("Unknown", Dinglemouse.getNumberName(-1));
     }
 }
