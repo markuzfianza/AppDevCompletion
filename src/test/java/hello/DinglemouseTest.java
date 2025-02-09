892b67d3f5a012b41943f4b284162949d8ca24e7
Reuben <2234208@slu.edu.ph>
2025-02-10 03:56:08 +0800

    Update on DinglemouseTest.java

--- src/test/java/hello/DinglemouseTest.java
+++ src/test/java/hello/DinglemouseTest.java
@@ -4,16 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DinglemouseTest {
 
     @Test
-    void testSingleDigitNumbers() {
-        assertEquals("One", Dinglemouse.getNumberName(1));
-        assertEquals("Two", Dinglemouse.getNumberName(2));
-        assertEquals("Three", Dinglemouse.getNumberName(3));
-        assertEquals("Zero", Dinglemouse.getNumberName(0));
+    void testOneYear() {
+        assertArrayEquals(new int[]{1, 15, 15}, Dinglemouse.humanYearsCatYearsDogYears(1));
     }
 
     @Test
-    void testUnknownNumber() {
-        assertEquals("Unknown", Dinglemouse.getNumberName(10));
-        assertEquals("Unknown", Dinglemouse.getNumberName(-1));
+    void testTwoYears() {
+        assertArrayEquals(new int[]{2, 24, 24}, Dinglemouse.humanYearsCatYearsDogYears(2));
+    }
+
+    @Test
+    void testTenYears() {
+        assertArrayEquals(new int[]{10, 56, 64}, Dinglemouse.humanYearsCatYearsDogYears(10));
     }
 }
