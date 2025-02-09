e427dc6029b35462015571903654c5106263bd26
Cyrus <2213259@slu.edu.ph>
2025-02-10 00:45:49 +0800

    fixed issue of GoodVsEvil.java

--- src/main/java/hello/GoodVsEvil.java
+++ src/main/java/hello/GoodVsEvil.java
@@ -2,37 +2,40 @@ package hello;
 
 public class GoodVsEvil {
 
-    public static String battle(String goodAmounts, String evilAmounts) {
-        String[] inputGood = goodAmounts.split(" ");
-        String[] inputEvil = evilAmounts.split(" ");
+    public static String determineBattleOutcome(String goodAmounts, String evilAmounts) {
+        int[] goodWorth = {1, 2, 3, 3, 4, 10};
+        int[] evilWorth = {1, 2, 2, 2, 3, 5, 10};
 
-        int hobbit = Integer.parseInt(inputGood[0]) * 1;
-        int men = Integer.parseInt(inputGood[1]) * 2;
-        int elves = Integer.parseInt(inputGood[2]) * 3;
-        int dwarves = Integer.parseInt(inputGood[3]) * 3;
-        int eagles = Integer.parseInt(inputGood[4]) * 4;
-        int wizard = Integer.parseInt(inputGood[5]) * 10;
+        int good = calculateScore(goodAmounts, goodWorth);
+        int evil = calculateScore(evilAmounts, evilWorth);
 
-        int good = hobbit + men + elves + dwarves + eagles + wizard;
+        if (good == -1 || evil == -1) {
+            return "Invalid input: Please ensure proper formatting.";
+        }
 
-        int orcs = Integer.parseInt(inputEvil[0]) * 1;
-        int evilMen = Integer.parseInt(inputEvil[1]) * 2;
-        int wargs = Integer.parseInt(inputEvil[2]) * 2;
-        int goblin = Integer.parseInt(inputEvil[3]) * 2;
-        int urukHai = Integer.parseInt(inputEvil[4]) * 3;
-        int trolls = Integer.parseInt(inputEvil[5]) * 5;
-        int evilwizard = Integer.parseInt(inputEvil[6]) * 10;
-
-        int evil = orcs + evilMen + wargs + goblin + urukHai + trolls + evilwizard;
-
-        if(good > evil){
+        if (good > evil) {
             return "Battle Result: Good triumphs over Evil";
-        }
-        else if (good < evil){
+        } else if (good < evil) {
             return "Battle Result: Evil eradicates all trace of Good";
-        }
-        else{
+        } else {
             return "Battle Result: No victor on this battle field";
         }
+    }
+
+    private static int calculateScore(String amounts, int[] worth) {
+        String[] values = amounts.split(" ");
+        if (values.length != worth.length) {
+            return -1; // Input validation failed
+        }
+
+        int totalScore = 0;
+        for (int i = 0; i < values.length; i++) {
+            try {
+                totalScore += Integer.parseInt(values[i]) * worth[i];
+            } catch (NumberFormatException e) {
+                return -1; // Handle invalid input (non-numeric values)
+            }
+        }
+        return totalScore;
     }
 }
