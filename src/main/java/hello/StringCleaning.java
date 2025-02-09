972a24c5c5ec754871f84f913ce6a0f95b7f4232
Reuben <2234208@slu.edu.ph>
2025-02-10 02:23:25 +0800

    Pairprogramming

---
+++ src/main/java/hello/StringCleaning.java
@@ -0,0 +1,11 @@
+public class StringCleaning {
+    static String stringClean(final String text) {
+        StringBuilder s = new StringBuilder();
+
+        for(char c : text.toCharArray())
+            if (!Character.isDigit(c)) {
+                s.append(c);
+            }
+        return s.toString();
+    }
+}
