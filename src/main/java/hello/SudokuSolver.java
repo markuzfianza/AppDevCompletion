fd3d95195daab2aeab5ed3a62890f05ea7c4bb25
Cyrus <2213259@slu.edu.ph>
2025-02-10 02:00:15 +0800

    Updated Files

--- src/main/java/hello/SudokuSolver.java
+++ src/main/java/hello/SudokuSolver.java
@@ -1,118 +1,60 @@
 package hello;
 
 public class SudokuSolver {
-    private static final int SIZE = 9; // Define the size of the Sudoku grid
- 
-    public static int[][] sudoku(int[][] b) {
-        if (solveSudoku(b)) {
-            return b; // Return the solved board
-        } else {
-            return null; // Return null if the puzzle cannot be solved
-        }
-    }
- 
-    private static boolean solveSudoku(int[][] b) {
-        int r = -1;
-        int c = -1;
-        boolean isVacant = true;
- 
-        // Find an empty cell
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (b[i][j] == 0) {
-                    r = i;
-                    c = j;
-                    isVacant = false;
-                    break;
+
+    public static int[][] sudoku(int[][] puzzle) {
+         solve(puzzle);
+      return puzzle;
+   }
+  
+    private static boolean solve(int[][] puzzle) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (puzzle[row][col] == 0) {  // Look for an empty cell
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(puzzle, row, col, num)) {
+                            puzzle[row][col] = num;  // Place num
+                            
+                            if (solve(puzzle)) {  // Recursively attempt to solve from here
+                                return true;
+                            } else {
+                                puzzle[row][col] = 0;  // Reset on failure
+                            }
+                        }
+                    }
+                    return false;  // Trigger backtracking
                 }
             }
-            if (!isVacant) {
-                break;
-            }
         }
- 
-        // If no empty cell is found, the puzzle is solved
-        if (isVacant) {
-            return true;
-        }
- 
-        // Otherwise, try filling the cell with numbers 1-9
-        for (int no = 1; no <= SIZE; no++) {
-            if (isSafe(b, r, c, no)) {
-                b[r][c] = no; // Place the number
- 
-                // Recursively attempt to solve the rest of the board
-                if (solveSudoku(b)) {
-                    return true;
-                }
- 
-                // If the guess didn't lead to a solution, reset the cell
-                b[r][c] = 0;
-            }
-        }
- 
-        return false; // Trigger backtracking
+        return true;  // Puzzle is solved
     }
- 
-    private static boolean isSafe(int[][] b, int row, int col, int num) {
-        // Check if the number is not in the current row
-        for (int x = 0; x < SIZE; x++) {
-            if (b[row][x] == num) {
+
+    private static boolean isSafe(int[][] puzzle, int row, int col, int num) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (puzzle[row][i] == num) {
                 return false;
             }
         }
- 
-        // Check if the number is not in the current column
-        for (int x = 0; x < SIZE; x++) {
-            if (b[x][col] == num) {
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (puzzle[i][col] == num) {
                 return false;
             }
         }
- 
-        // Check if the number is not in the current 3x3 box
-        int boxRowStart = row - row % 3;
-        int boxColStart = col - col % 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (b[i + boxRowStart][j + boxColStart] == num) {
+
+        // Check 3x3 sub-grid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (puzzle[i][j] == num) {
                     return false;
                 }
             }
         }
- 
-        return true; // Safe to place the number
+
+        return true;  // No conflicts, safe to place num
     }
- 
-    public static void main(String[] args) {
-        int[][] board = {
-                {5, 3, 0, 0, 7, 0, 0, 0, 0},
-                {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                {0, 0, 0, 0, 8, 0, 0, 7, 9}
-        };
- 
-        System.out.println("Original Sudoku:");
-        printBoard(board);
- 
-        int[][] solvedBoard = sudoku(board);
- 
-        System.out.println("\nSolved Sudoku:");
-        printBoard(solvedBoard);
-    }
- 
-    private static void printBoard(int[][] board) {
-        for (int r = 0; r < SIZE; r++) {
-            for (int d = 0; d < SIZE; d++) {
-                System.out.print(board[r][d] + " ");
-            }
-            System.out.print("\n");
-        }
-    }
- }
- 
- 
+}
