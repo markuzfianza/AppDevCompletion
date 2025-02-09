71f9d32efa81f2fd1d22dd502c6322cb920c83b2
Reuben <2234208@slu.edu.ph>
2025-02-10 04:35:34 +0800

    editted the imports of DinglemouseTest

--- src/test/java/hello/DinglemouseTest.java
+++ src/test/java/hello/DinglemouseTest.java
@@ -8,16 +8,16 @@ public class DinglemouseTest {
 
     @Test
     void testOneYear() {
-        assertArrayEquals(new int[]{1, 15, 15}, Dinglemouse.humanYearsCatYearsDogYears(1));
+        assertArrayEquals(new int[]{1, 15, 15}, Dinglemouse.convertHumanToAnimalYears(1));
     }
 
     @Test
     void testTwoYears() {
-        assertArrayEquals(new int[]{2, 24, 24}, Dinglemouse.humanYearsCatYearsDogYears(2));
+        assertArrayEquals(new int[]{2, 24, 24}, Dinglemouse.convertHumanToAnimalYears(2));
     }
 
     @Test
     void testTenYears() {
-        assertArrayEquals(new int[]{10, 56, 64}, Dinglemouse.humanYearsCatYearsDogYears(10));
+        assertArrayEquals(new int[]{10, 56, 64}, Dinglemouse.convertHumanToAnimalYears(10));
     }
 }
